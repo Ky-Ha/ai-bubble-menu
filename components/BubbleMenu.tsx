7ded0f85056c0783d7093a93924f9951ccf3846b
Ky-Ha <kyjob2611@gmail.com>
2025-09-21 04:37:10 +0700

    simple bubble menu

---
+++ components/BubbleMenu.tsx
@@ -0,0 +1,32 @@
+import { MessageCircleQuestionMark } from 'lucide-react'
+import { EditorBubble, useEditor } from 'novel'
+
+const GenerativeMenuSwitch = () => {
+  const { editor } = useEditor()
+
+  if (!editor) return <></>
+
+  const askAIAssistant = () => {
+    console.log('Ask AI')
+  }
+
+  return (
+    <EditorBubble
+      tippyOptions={{
+        appendTo: () => document.body,
+        interactive: true,
+        placement: 'bottom',
+      }}
+      shouldShow={({ from, to }) => from !== to}
+    >
+      <div className="bg-yellow-300 p-2 rounded flex flex-row gap-x-1">
+        <MessageCircleQuestionMark />
+        <button onClick={askAIAssistant}>
+          Ask Ai
+        </button>
+      </div> 
+    </EditorBubble>
+  )
+}
+
+export default GenerativeMenuSwitch
